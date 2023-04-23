8bb26e9c42ffedabb71ada44bead0454b6cf5611
DoctorManhattan123 <alexandervonberkov1@gmail.com>
2023-04-23 18:03:33 +0200

    feat: add some lines

--- rust_lib/src/lib.rs
+++ rust_lib/src/lib.rs
@@ -1,8 +1,11 @@
 // This includes the automatically generated bindings (generated with bindgen)
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+include!(concat!(env!("CARGO_MANIFEST_DIR"), "/generated/bindings.rs"));
 
 #[no_mangle]
-pub extern "C" fn rust_function(x: i32) {
-    println!("Hello from Rust! Received: {}", x);
-    unsafe { hello_from_cpp(3.14) };
+pub extern "C" fn rust_function() {
+    println!("Hello from Rust!");
+    unsafe {
+        hello_from_cpp(3.14);
+        another_cpp_function(42);
+    }
 }
